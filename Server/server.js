335bac311760e7ae2eb62eb5d234d0b37b402b8f
Gurmehak Kaur <gurmehakkaur52@gmail.com>
2024-10-23 15:10:16 +0530

    home and user page updated

--- Server/server.js
+++ Server/server.js
@@ -17,11 +17,27 @@ app.use(cors());
 // error handling middleware
 app.use(errorHandler);
 
+app.set('view engine', 'hbs');
+
 // routes
 app.get('/', (req, res) => {
     res.send("working");
 });
+app.get("/home", (req, res) => {
+    // let user = User.findOne({id:})
+    res.render("Home", {
+        "username":"Gurmehak",
+    })
+})
+app.get("/users", (req, res) => {
+    res.render("Users", {
+        users:[
+            {id: 1, username:"Gurmehak", age: 20}, 
+            {id: 2, username: "xyz", age: 21}
+        ]
 
+    })
+})
 // APP CONFIG START
 app.listen(port, () => {
     console.log(`Server running on port http://localhost:${port}`);
